80d4f21fc3b424c2a5ec0cad3ba43c2f410280e7
Rohail Iqbal <rohail.pucit@gmail.com>
2021-01-21 20:45:13 +0500

    Get Data from Web API through Angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            //Configuring CORS which is a security mechanism
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -56,6 +58,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            //CORS middleware should be configured after Routing middleware.
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
